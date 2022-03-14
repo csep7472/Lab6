990189c9305d8eeeab0dfd0e2cf4e35516ad7e56
Colin Sepulveres <colinsep@gmail.com>
2022-03-14 19:28:30 -0400

    add or subtract numbers

--- IdeaProjects/ProgFund1/src/Lab6.java
+++ IdeaProjects/ProgFund1/src/Lab6.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 public class Lab6 {
     public static void main(String[] args){
@@ -6,7 +7,14 @@ public class Lab6 {
         int x = input.nextInt();
         System.out.println("Enter your second number: ");
         int y = input.nextInt();
-        int total = x + y;
+        System.out.println("Add or Subtract?");
+        String choice = input.next();
+        int total;
+        if (Objects.equals(choice, "add")) {
+            total = x + y;
+        } else{
+            total = x - y;
+        }
         System.out.println(total);
     }
 }
